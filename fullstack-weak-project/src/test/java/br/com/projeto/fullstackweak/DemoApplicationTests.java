64d56cb712d82ca2bf8ed5030a173ec63d2aae93
Bruno Bandeira <brunodev29ab@gmail.com>
2021-03-28 01:36:16 -0300

    Inicio do projeto

---
+++ fullstack-weak-project/src/test/java/br/com/projeto/fullstackweak/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.fullstackweak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
